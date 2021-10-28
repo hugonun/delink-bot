d2a3b3000830dd2e291af2ebb3937dd013f4b8b3
unknown <hugonunes@email.com>
2021-10-28 20:07:39 +0200

    Cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-# delink-bot
-A Discord bot to combat phishing links for Steam trades and Discord gifts.
--- functions.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def findurls(s):
-
-      regex = r"(?i)\b(((https?|ftp|smtp):\/\/)?(www.)?[a-zA-Z0-9_.-]+\.[a-zA-Z0-9_.-]+(\/[a-zA-Z0-9#]+\/?)*/*)$"
-      url = re.findall(regex,s)
-      return [x[0] for x in url]
