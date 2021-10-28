9a38fe0207863d55ab040c0a64906fc9de253f7d
hugonun <43740923+hugonun@users.noreply.github.com>
2021-10-28 04:50:06 +0200

    Create init.py

---
+++ init.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+description = '''Link filter bot.
+'''
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!d ', help_command=None, description=description, intents=intents)
+
+@bot.event
+async def on_ready():
+  print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+  print('------')
+  game = discord.Game("word.exe")
+  await bot.change_presence(status=discord.Status.online, activity=game)
+
+@bot.command()
+async def ping(ctx):
+  await ctx.send('Pong!')
+
+@bot.event
+async def on_message(message):
+  # read blacklist.txt and whitelist.txt, and filter from there
+  # remove all domains using .gift TLD
+  if 0=1:
+    await ctx.send('Possible scam link has been deleted!', delete_after=5)
+  
+
+bot.run('')
