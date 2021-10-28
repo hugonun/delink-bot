7a79473956a2704e01055922d994f48865540afa
unknown <hugonunes@email.com>
2021-10-28 18:35:40 +0200

    Code Cleanup

---
+++ functions.py
@@ -0,0 +1,7 @@
+import re
+
+def findurls(s):
+
+      regex = r"(?i)\b(((https?|ftp|smtp):\/\/)?(www.)?[a-zA-Z0-9_.-]+\.[a-zA-Z0-9_.-]+(\/[a-zA-Z0-9#]+\/?)*/*)$"
+      url = re.findall(regex,s)
+      return [x[0] for x in url]
--- init.py
+++ init.py
@@ -2,6 +2,8 @@ from re import findall
 import discord
 from discord.ext import commands
 
+from functions import *
+
 description = '''Link filter bot.
 '''
 
@@ -25,26 +27,15 @@ async def ping(ctx):
 async def on_message(message):
   # read blacklist.txt and whitelist.txt, and filter from there
   # remove all domains using .gift TLD
-  import re
-    
-  def Find(string):
-    
-      # findall() has been used 
-      # with valid conditions for urls in string
-      regex = r"(?i)\b(((https?|ftp|smtp):\/\/)?(www.)?[a-zA-Z0-9_.-]+\.[a-zA-Z0-9_.-]+(\/[a-zA-Z0-9#]+\/?)*/*)$"
-      url = re.findall(regex,string)
-      return [x[0] for x in url]
-        
-  # Driver Code
-  textToTest = message.content
-  # print("Urls: ", Find(textToTest))
-  listOfURLs = Find(textToTest)
+  urllist = findurls(message.content)
+  print(urllist)
   
   if 0==1:
     await ctx.send('Possible scam link has been deleted!', delete_after=5)
-  
+    
+
+# Token
 with open('token.txt', 'r') as file:
     dtoken = file.read().replace('\n', '')
-
 
 bot.run(dtoken)
