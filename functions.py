42b4901ba9cee6bc2d673bd5e7416f3768446fbb
hugonun <43740923+hugonun@users.noreply.github.com>
2021-11-01 06:06:52 +0100

    Updated and cleaned fuctions.py

--- functions.py
+++ functions.py
@@ -1,47 +1,41 @@
 import re
 import sqlite3
 
+import tldextract
 from discord.ext.commands.core import guild_only
 con = sqlite3.connect('delink.db')
 cur = con.cursor()
 
 def findurls(s):
-      regex = r"(?i)\b(((https?|ftp|smtp):\/\/)?(www.)?[a-zA-Z0-9_.-]+\.[a-zA-Z0-9_.-]+(\/[a-zA-Z0-9#]+\/?)*/*)$"
-      url = re.findall(regex,s)
-      return [x[0] for x in url]
+  regex = r"(?i)\b(((https?|ftp|smtp):\/\/)?(www.)?[a-zA-Z0-9_.-]+\.[a-zA-Z0-9_.-]+(\/[a-zA-Z0-9#]+\/?)*/*)$"
+  url = re.findall(regex,s)
+  return [x[0] for x in url]
 
 async def deletemsg(message):
-    await message.delete()
-    await message.channel.send('WARNING: Your message has been deleted for containing a possible scam URL.', delete_after=5)
+  await message.delete()
+  await message.channel.send('WARNING: Your message has been deleted for containing a possible scam URL.', delete_after=5)
 
 def setupdb():
-      cur.execute('''CREATE TABLE IF NOT EXISTS blacklist
-      (guild_id text, url text, UNIQUE(guild_id, url))''')
+  cur.execute('''CREATE TABLE IF NOT EXISTS blacklist
+  (guild_id int, url text, UNIQUE(guild_id, url))''')
 
-      cur.execute('''CREATE TABLE IF NOT EXISTS whitelist
-      (guild_id text, url text, UNIQUE(guild_id, url))''')
+  cur.execute('''CREATE TABLE IF NOT EXISTS whitelist
+  (guild_id int, url text, UNIQUE(guild_id, url))''')
 
-      con.commit()
+  con.commit()
 
-def inserturls(guild_id, urls, table):
-      def valuestoinsert(url_pool):
-            for url in url_pool:
-                  yield (guild_id, url)
-      cur.executemany('''INSERT OR IGNORE INTO %s VALUES (?,?)''' % (table), valuestoinsert(urls))
+def inserturls(guild_id, url, table):
+  cur.execute('''INSERT OR IGNORE INTO %s VALUES (?,?)''' % (table), (guild_id,url))
+  con.commit()
       
-      con.commit()
-      
-def deleteurls(guild_id, urls, table):
-      def valuestoinsert(url_pool):
-            for url in url_pool:
-                  yield (guild_id, url)
-      cur.executemany('''DELETE FROM %s WHERE url = ? AND guild_id = ?''' % (table),valuestoinsert(urls))
-      
-      con.commit()
+def deleteurls(guild_id, url, table):
+  cur.execute('''DELETE FROM %s WHERE url = ? AND guild_id = ?''' % (table), (guild_id,url))
+  con.commit()
 
 def retriveurls(guild_id, table):
+  cur.execute('''SELECT url FROM %s WHERE guild_id = ?''' % (table), (guild_id,))
+  return cur.fetchall()
 
-      print(len(guild_id))
-      cur.execute('''SELECT url FROM %s WHERE guild_id = ?''' % (table), (guild_id))
-
-      return cur.fetchall()
+def checkurl(guild_id, url, table):
+  cur.execute('''SELECT url FROM %s WHERE guild_id = ? AND url = ?''' % (table), (guild_id,url))
+  return cur.fetchone()
