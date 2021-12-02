dde3fa8ca2c59dca55b20d4cc23fa6fc9397d279
hugonun <43740923+hugonun@users.noreply.github.com>
2021-12-02 19:55:16 +0100

    Add invite and support link

--- cogs/info.py
+++ cogs/info.py
@@ -13,8 +13,17 @@ class Info(commands.Cog):
         start_time = time.time()
         message = await ctx.send("Testing Ping...")
         end_time = time.time()
-
         await message.edit(content=f"Pong! {round(self.bot.latency * 1000)}ms\nAPI: {round((end_time - start_time) * 1000)}ms")
+        
+    @commands.command()
+    async def invite(self, ctx):
+        """Invite the bot to your server."""
+        await ctx.send("https://discord.com/oauth2/authorize?client_id=903114852631969802&scope=bot&permissions=2147871808")
+        
+    @commands.command()
+    async def support(self, ctx):
+        """Join our support server."""
+        await ctx.send("https://discord.com/invite/Gf3panp2bw")
 
 
 def setup(bot):
